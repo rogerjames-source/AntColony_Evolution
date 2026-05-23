7676f665d060d48e1d33502d7d2f01fc3e00d48a
rogerjames-source <abanterogerjames@gmail.com>
2026-05-24 00:40:48 +0800

    Add files via upload

---
+++ Game/src/state/Difficulty.java
@@ -0,0 +1,7 @@
+package state;
+
+public enum Difficulty {
+    EASY,
+    HARD,
+    INSANE
+}
---
+++ Game/src/state/GameState.java
@@ -0,0 +1,11 @@
+package state;
+
+public enum GameState {
+    COVER_PAGE,
+    DIFFICULTY_MENU,
+    SETTINGS_MENU,
+    PLAYING,
+    EVOLUTION_SELECT,
+    GAME_OVER,
+    SHOWCASE_MENU
+}
